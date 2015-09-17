b07ca95af774ff0046ab6de38e23ff4c38f8575e
ducit <ducromrom12101995@gmail.com>
2015-09-17 13:18:52 +0700

    test

---
+++ test.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        
+        <script language="javascript">  
+        var value = '';
+        var a=1;
+        document.write(a);
+             </script>
+    </body>
+</html>
